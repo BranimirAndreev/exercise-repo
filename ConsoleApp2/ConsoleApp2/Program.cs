1fd328568d46a9f60fc42486995dae1b03d5fee0
branimir andreev <br_andreev@abv.bg>
2017-10-01 22:11:47 +0300

    proba

---
+++ ConsoleApp2/ConsoleApp2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the lenghts of the rectangle");
+            var a = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the width of the rectangle");
+            var b = decimal.Parse(Console.ReadLine());
+            Console.WriteLine("The area of the rectangle is ");
+            var c = a * b;
+            Console.WriteLine(c);
+
+        }
+    }
+    
+    }
